dc1817a39f9cac1306235df789858651878b9f34
gfandos <gerard.fandos@gmail.com>
2017-02-15 18:03:30 +0100

    Added xml log a part from the txt

--- src/SocketCliente.java
+++ src/SocketCliente.java
@@ -19,6 +19,19 @@
  * de recursos que puede ser solicitados.
  */
 
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 import java.io.*;
 import java.net.InetSocketAddress;
 import java.net.Socket;
@@ -29,7 +42,7 @@ public class SocketCliente {
 
     private static String ip = "0.0.0.0";
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws TransformerException, ParserConfigurationException, SAXException {
 
         System.out.println("Creado el socket cliente");
         Socket clienteSocket = new Socket();
@@ -80,6 +93,7 @@ public class SocketCliente {
             String toLog = "Petición de: " + ip + ". A fecha: " + date.toString() + ". Operación: " + mensaje + " = " + resultString + "\n";
 
             writeLog(toLog);
+            writeXMLLog(ip, date.toString(), mensaje + " = " + resultString);
 
             clienteSocket.close();
 
@@ -103,6 +117,66 @@ public class SocketCliente {
         writer.append(result);
         writer.newLine();
         writer.close();
+
+    }
+    public static void writeXMLLog(String ip, String date, String operacion) throws IOException, ParserConfigurationException, TransformerException, SAXException {
+
+        DocumentBuilderFactory docFactory = DocumentBuilderFactory.newInstance();
+        DocumentBuilder docBuilder = docFactory.newDocumentBuilder();
+        Document doc = null;
+        File f = new File("xmlLog");
+        if(f.exists()){
+
+            doc = docBuilder.parse("xmlLog");
+            Node rootElement = doc.getElementsByTagName("log").item(0);
+
+            Element operation = doc.createElement("operation");
+            rootElement.appendChild(operation);
+
+            Element sender = doc.createElement("ip");
+            sender.appendChild(doc.createTextNode(ip));
+            operation.appendChild(sender);
+
+            Element data = doc.createElement("date");
+            data.appendChild(doc.createTextNode(date));
+            operation.appendChild(data);
+
+            Element operat = doc.createElement("formula");
+            operat.appendChild(doc.createTextNode(operacion));
+            operation.appendChild(operat);
+
+        } else {
+
+            doc = docBuilder.newDocument();
+            Element rootElement = doc.createElement("log");
+            doc.appendChild(rootElement);
+
+            Element operation = doc.createElement("operation");
+            rootElement.appendChild(operation);
+
+            Element sender = doc.createElement("ip");
+            sender.appendChild(doc.createTextNode(ip));
+            operation.appendChild(sender);
+
+            Element data = doc.createElement("date");
+            data.appendChild(doc.createTextNode(date));
+            operation.appendChild(data);
+
+            Element operat = doc.createElement("formula");
+            operat.appendChild(doc.createTextNode(operacion));
+            operation.appendChild(operat);
+
+            // http://stackoverflow.com/questions/4142046/create-xml-file-using-java
+
+
+        }
+
+        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+        Transformer transformer = transformerFactory.newTransformer();
+        DOMSource source = new DOMSource(doc);
+
+        StreamResult result =  new StreamResult(new File("xmlLog"));
+        transformer.transform(source, result);
 
     }
 }
